c765b6a51ac4c788fc4759fb04feadd19e1638fe
rajangdavis <rajangdavis@gmail.com>
2018-04-16 17:29:12 -0700

    refactored a little bit, but need to start building out the normalize portion of this (Hope it isn't too crazy)

--- cmd/query.go
+++ cmd/query.go
@@ -121,7 +121,7 @@ func CheckRequiredFlags(flags *pflag.FlagSet) error {
 var query = &cobra.Command{
 	Use: "query",
 	Short: "Runs one or more ROQL queries",
-	Long: "Runs one or more ROQL queries",
+	Long: "\033[93mRuns one or more ROQL queries\033[0m \033[0;32m\n\nSingle Query Example: \033[0m \n$ osvc-rest query \"DESCRIBE\" -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE \033[0;32m\n\nMultiple Queries Example:\033[0m \n$ osvc-rest query \"SELECT * FROM INCIDENTS LIMIT 100\" \"SELECT * FROM SERVICEPRODUCTS LIMIT 100\" \"SELECT * FROM SERVICECATEGORIES LIMIT 100\" -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE",
 	PreRunE: func(cmd *cobra.Command, args []string) error {		
 		return CheckRequiredFlags(cmd.Flags())
 	},
