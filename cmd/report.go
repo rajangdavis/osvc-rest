b93a01e95b0556ec438fe04bbb6b9d378d90ca8b
rajangdavis <rajangdavis@gmail.com>
2018-05-27 03:03:05 -0700

    added offset[1;5D[1;5D[1;5D[1;5D[1;5D[1;5D--offset flag to reports

--- cmd/report.go
+++ cmd/report.go
@@ -9,7 +9,7 @@ import (
 )
 
 var lookupName, filters string
-var id, reportLimit int
+var id, reportLimit, reportOffset int
 
 func checkReportFlags(flags *pflag.FlagSet) error {
 
@@ -36,6 +36,10 @@ func runReport(cmd *cobra.Command, args []string) error {
 		str = str[:len(str)-1] + fmt.Sprintf(`, "limit" : %d}`, reportLimit)
 	}
 
+	if reportOffset > 0 {
+		str = str[:len(str)-1] + fmt.Sprintf(`, "offset" : %d}`, reportOffset)
+	}
+
 	if filters != "" {
 		str = str[:len(str)-1] + fmt.Sprintf(`, "filters" : %s}`, filters)
 	}
@@ -54,7 +58,7 @@ func runReport(cmd *cobra.Command, args []string) error {
 var report = &cobra.Command{
 	Use:   "report",
 	Short: "Runs an analytics report command",
-	Long:  "\033[93mRuns an analytics report and returns parsed results\033[0m \033[0;32m\n\nReport (without filters) Example: \033[0m \n$ osvc-rest report --id 176 -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE \033[0;32m\n\nReport (with filters and limiting) Example: \033[0m \n$ osvc-rest report --id 176 --limit 10 --filters '[{\"name\":\"search_ex\",\"values\":\"returns\"}]' -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE",
+	Long:  "\033[93mRuns an analytics report and returns parsed results\033[0m \033[0;32m\n\nReport (without filters) Example: \033[0m \n$ osvc-rest report --id 176 -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE \033[0;32m\n\nReport (with filters, offsetting and limiting results) Example: \033[0m \n$ osvc-rest report --id 176 --limit 10 --offset 10 --filters '[{\"name\":\"search_ex\",\"values\":\"returns\"}]' -u $OSC_ADMIN -p $OSC_PASSWORD -i $OSC_SITE",
 	PreRunE: func(cmd *cobra.Command, args []string) error {
 		return checkReportFlags(cmd.Flags())
 	},
@@ -65,6 +69,7 @@ func init() {
 	report.Flags().StringVarP(&filters, "filters", "f", "", "Adds filters for reporting")
 	report.Flags().StringVarP(&lookupName, "name", "n", "", "Sets the lookupName of the AnalyticsReport that we wish to run")
 	report.Flags().IntVarP(&reportLimit, "limit", "l", 0, "Adds limit for reporting")
+	report.Flags().IntVarP(&reportOffset, "offset", "o", 0, "Adds and offset for reporting")
 	report.Flags().IntVarP(&id, "id", "", 0, "Sets the id of the AnalyticsReport that we wish to run")
 	RootCmd.AddCommand(report)
 }
